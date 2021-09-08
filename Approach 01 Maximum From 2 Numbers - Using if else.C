3d626842be8b64d5c3fe3bea9d9c4433a4b44271
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-09-08 11:48:03 +0530

    Add files via upload

---
+++ Approach 01 Maximum From 2 Numbers - Using if else.C
@@ -0,0 +1,26 @@
+// Maximum From 2Numbers - Using if else
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+     int Num1 = 0,Num2 = 0;
+
+    Up:
+     printf("\n Enter 2 Integer Numbers = ");
+     scanf("%d%d",&Num1,&Num2);
+
+     if(Num1>Num2)
+     {
+        printf("\n Number %d is Maximmum",Num1);
+     }
+     else
+     {
+         printf("\n Number %d is Maximum",Num2);
+     }
+     goto Up;
+
+     getch();
+     return 0;
+}
---
+++ Approach 03 Maximum From 2 Numbers - Using if else.C
@@ -0,0 +1,27 @@
+// Maximum From 2Numbers - Using if else
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     int Num1 = 0,Num2 = 0;
+
+     printf("\n Enter 2 Integer Numbers = ");
+     scanf("%d%d",&Num1,&Num2);
+
+     if(Num1 == Num2)
+     {
+        printf("\n Both Given Numbers Are Equal.");
+     }
+     else if(Num1 > Num2)
+     {
+         printf("\n Number %d is Maximum.",Num1);
+     }
+     else
+     {
+         printf("\n Number %d is Maximum.",Num2);
+     }
+
+     getch();
+     return 0;
+}
