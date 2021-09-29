d98ac858e4d6dc852d8c06daf89c997ca40b9e0a
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-09-29 23:05:44 +0530

    Add files via upload

---
+++ 25 Pattarn Left Upper Character trangular.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0,j=0,c=0;
+
+    printf("\n Enter Value For Print Pattarn = ");
+    scanf("%d",&c);
+
+    {
+        for(i=1;i<=c;i++)
+        {
+            for(j=1;j<=c;j++)
+            {
+            if(i>=j)
+            {
+                printf("%d",i);
+
+            }
+            else
+            {
+                printf(" ");
+            }
+
+            }
+
+            printf("\n");
+        }
+
+    }
+    getch();
+    return 0;
+}
