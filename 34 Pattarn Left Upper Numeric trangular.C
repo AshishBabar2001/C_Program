13bd17671a6e391104605d63e2f283fa3b6873a1
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-10-07 18:24:20 +0530

    Add files via upload

---
+++ 34 Pattarn Left Upper Numeric trangular.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0,j=0,c=0,Num=0;
+
+    printf("\n Enter Value For Print Pattarn = ");
+    scanf("%d",&c);
+
+    printf("\n Enter a Number To Get its Table in Pattarn = ");
+    scanf("%d",&Num);
+
+    {
+        for(i=1;i<=c;i++)
+        {
+            for(j=1;j<=c;j++)
+            {
+            if(i>=j)
+            {
+                printf("%3d",Num);
+                Num = Num+Num;
+            }
+            else
+            {
+                printf(" ");
+            }
+            }
+            printf("\n");
+        }
+
+    }
+    getch();
+    return 0;
+}
