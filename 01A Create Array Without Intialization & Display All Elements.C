9f2ed6c9098fe440231881910132d340506495e2
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-10-08 14:47:39 +0530

    Add files via upload

---
+++ 01A Create Array Without Intialization & Display All Elements.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[5]={};
+
+    printf("\n Value of Array Elements Are :-");
+
+    printf("\n Value of 101 Element = %d.",Arr[0]);
+    printf("\n Value of 102 Element = %d.",Arr[1]);
+    printf("\n Value of 103 Element = %d.",Arr[2]);
+    printf("\n Value of 104 Element = %d.",Arr[3]);
+    printf("\n Value of 105 Element = %d.",Arr[4]);
+
+    getch();
+    return 0;
+}
