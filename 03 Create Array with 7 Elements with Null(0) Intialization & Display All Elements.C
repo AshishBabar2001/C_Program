47bab0e716c8dc706ccb21668185868f794b00d8
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-10-09 23:54:44 +0530

    Add files via upload

---
+++ 03 Create Array with 7 Elements with Null(0) Intialization & Display All Elements.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[7]={};
+    int i = 0;
+
+    printf("\n Value of Array Elements Are :- ");
+
+    for(i=0;i<7;i++)
+    {
+        printf("\n Value of 101 Element = %d.",Arr[i]);
+    }
+
+    getch();
+    return 0;
+}
