3c2cef5574f3fd3fbeae9e540cc0f665b25a888b
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-10-13 18:03:29 +0530

    Add files via upload

---
+++ 04 Create Array with 7 Elements with Null(0) Intialization & Display All Elements without Loop.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[7]={};
+
+    printf("\n Enter Element To Display Array = ");
+    scanf("%d",Arr[0]);
+    printf("\n Enter Element To Display Array = ");
+    scanf("%d",Arr[1]);
+    printf("\n Enter Element To Display Array = ");
+    scanf("%d",Arr[3]);
+    printf("\n Enter Element To Display Array = ");
+    scanf("%d",Arr[4]);
+    printf("\n Enter Element To Display Array = ");
+    scanf("%d",Arr[5]);
+    printf("\n Enter Element To Display Array = ");
+    scanf("%d",Arr[6]);
+
+    getch();
+
+    printf("\n Value of Array Elements Are :- ");
+
+    printf("\n Value of 101 Element = %d.",Arr[0]);
+    printf("\n Value of 102 Element = %d.",Arr[1]);
+    printf("\n Value of 103 Element = %d.",Arr[2]);
+    printf("\n Value of 104 Element = %d.",Arr[3]);
+    printf("\n Value of 105 Element = %d.",Arr[4]);
+    printf("\n Value of 106 Element = %d.",Arr[5]);
+    printf("\n Value of 107 Element = %d.",Arr[6]);
+
+    getch();
+    return 0;
+}
