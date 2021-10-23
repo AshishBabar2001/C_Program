13402941312898f121466607cb06a4e932d09651
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-10-23 16:27:22 +0530

    Add files via upload

---
+++ 01 Program to Print Character or Alphabtes in A To Y.C
@@ -0,0 +1,16 @@
+//Program to Print Character/Alphabtes in A To Y.C
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch='/0';
+     for(ch='A';ch<='Y';ch++)
+     {
+         printf("\n %c",ch);
+     }
+
+     getch();
+     return 0;
+
+}
