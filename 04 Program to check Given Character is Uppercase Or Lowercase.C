7c8f82320923e799288f1d76c8e592f84e516e2e
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-10-23 16:28:24 +0530

    Add files via upload

---
+++ 04 Program to check Given Character is Uppercase Or Lowercase.C
@@ -0,0 +1,13 @@
+//Program to check Given Character is Uppercase Or Lowercase
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+    char uk='\0';
+    printf("\n Enter a Character to Check Wheather it is Uppercase Or Lowercase = ");
+    scanf("%c",&uk);
+
+     getch();
+     return 0;
+ }
