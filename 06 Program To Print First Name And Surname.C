1e369317f0e4c6d52d5d291b9c283a42d9801de9
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-10-25 22:04:00 +0530

    Add files via upload

---
+++ 06 Program To Print First Name And Surname.C
@@ -0,0 +1,17 @@
+//Program To Print First Name And Surname
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("\n ASHISH ");
+        printf("\n  BABAR \n");
+    }
+
+    getch();
+    return 0;
+
+}
