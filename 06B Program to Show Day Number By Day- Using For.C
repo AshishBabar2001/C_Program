5ffbb4c00308df7ee2ddc1a91cd68ea0d3f03b69
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-10-25 22:04:40 +0530

    Add files via upload

---
+++ 06B Program to Show Day Number By Day- Using For.C
@@ -0,0 +1,18 @@
+//Program To Accept Value & Its Corresponding Character
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0;
+    while(i<=5)
+    {
+        printf("\n Welcome ");
+        printf("\n  TO India \n");
+        i++;
+    }
+
+    getch();
+    return 0;
+
+}
