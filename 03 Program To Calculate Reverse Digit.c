5b6c089e42617c8531580586db92c9922aecfe27
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-10-28 23:01:52 +0530

    Add files via upload

---
+++ 03 Program To Calculate Reverse Digit.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0,Dig=0,Temp=0,dRev=0;
+
+    printf("\n Enter +ve Integer Number To Calculate Its Reverse : ");
+    scanf("%d",&No);
+
+    Temp = No;
+
+    if(No<0)
+    {
+        printf("Invalid Number"); return -1;
+    }
+
+    while(Temp>0)
+    {
+        Dig = Temp%10;
+        dRev = (dRev*10)+Dig;
+        Temp = Temp/10;
+    }
+
+    printf("\n Sum of Digits In Given Number %d is = %d",No,dRev);
+
+    getch();
+    return 0;
+
+}
