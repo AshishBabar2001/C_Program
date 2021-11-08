f483890e3af5f18b6cd0d6a605dab7668c188fdc
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-08 15:37:57 +0530

    Add files via upload

---
+++ 08 Program To Count No Sum Of Digits In Given Number - Using For Loop.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0,Temp=0,dSum=0;
+
+    printf("\n Enter +ve Integer Number To Calculate Its Even Digits : ");
+    scanf("%d",&No);
+
+    Temp = No;
+
+    if(No<0)
+    {
+        printf("\n Invalid Number ");return -1;
+    }
+
+    for(Temp = No;Temp>0;Temp/=10)
+    {
+        printf("\n %d + %d = %d",dSum,(Temp%10),(dSum+(Temp%10)));
+        dSum += (Temp%10);
+    }
+
+    printf("\n\n Sum Of Digits In Given Number %d is = %d",No,dSum);
+
+    getch();
+    return 0;
+
+}
