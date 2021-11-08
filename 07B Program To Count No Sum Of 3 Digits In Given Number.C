9f0cc1412f4c3d676f78ef6c5e9a8fb03508a949
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-08 15:37:48 +0530

    Add files via upload

---
+++ 07B Program To Count No Sum Of 3 Digits In Given Number.C
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0,Temp=0,dCnt=0,dSum=0;
+
+    printf("\n Enter +ve Integer Number To Calculate Its Even Digits : ");
+    scanf("%d",&No);
+
+    Temp = No;
+
+    if(No<0)
+    {
+        printf("\n Invalid Number ");return -1;
+    }
+
+   while(Temp > 0)
+   {
+       dCnt++;
+       Temp = Temp / 10;
+   }
+   if(dCnt == 3)
+   {
+       Temp = No ;
+
+       while(Temp > 0)
+       {
+           dSum = dSum + Temp % 10;
+           Temp /= 10;
+       }
+       printf("\n Sum Of Digits In Given Number %d is = %d",No,dSum);
+   }
+   else
+   {
+       printf("\n Given Number Is Not 3 Digit Number :");
+   }
+
+    getch();
+    return 0;
+
+}
