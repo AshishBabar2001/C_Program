d64519b31e74623e01b577c23caa68c924962f71
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-13 17:15:53 +0530

    Add files via upload

---
+++ 01 Approach Number is Positive or Negative.C
@@ -0,0 +1,24 @@
+//Number is Positive Or Negative
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num = 0;
+
+    printf("\n Enter an Integer Number = ");
+    scanf("%d",&Num);
+
+    if(Num>0)
+    {
+        printf("\n Given Number is +ve.");
+    }
+    else
+    {
+        printf("\n Given Number is -ve.");
+    }
+
+    getch();
+    return 0;
+
+}
