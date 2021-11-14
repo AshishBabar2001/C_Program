064f438c213fc3bc8a27e9fba969d2c35db62abb
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-15 00:23:44 +0530

    Add files via upload

---
+++ 05 Use of Size of() Operator.C
@@ -0,0 +1,16 @@
+//Use Of Sizeof( )Operator
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n Size For Integer in This Environment = %d.",sizeof(int));
+    printf("\n Size For Integer in This Environment = %d.",sizeof(char));
+    printf("\n Size For Integer in This Environment = %d.",sizeof(short int));
+    printf("\n Size For Integer in This Environment = %d.",sizeof(float));
+    printf("\n Size For Integer in This Environment = %d.",sizeof(double));
+
+    getch();
+    return 0;
+
+}
